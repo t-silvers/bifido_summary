33e08076d8ce992ee57d36a1457bfbe864e8425a
t-silvers <thomas.silvers@me.com>
2024-11-14 16:03:05 +0100

    refactor scripts

--- workflow/scripts/create_barplot_data.sql
+++ workflow/scripts/create_barplot_data.sql
@@ -1,3 +1,5 @@
+-- duckdb -readonly /ptmp/thosi/bifido_summary/data/variants/Bifidobacterium_longum.duckdb
+
 set memory_limit = getenv('MEMORY_LIMIT');
 set threads = getenv('SLURM_CPUS_PER_TASK');
 set enable_progress_bar = false;
@@ -7,5 +9,6 @@ select
 
 from
     read_csv(
-        
+        '/ptmp/thosi/bifido_summary/results/Bifidobacterium_longum/annot_filtered_calls.csv'
     )
+;
